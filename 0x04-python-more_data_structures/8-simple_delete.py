9b22c74d8e740cec0f33f7de5243478653dda48a
FarieB <faraibande@gmail.com>
2023-12-11 11:03:42 -0800

    8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-
-    new_dict = dict(a_dictionary)  # Create a copy to avoid modifying the original
-    new_dict.pop(key, None)  # Remove the key if it exists
-    return new_dict
-
-if __name__ == "__main__":
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
-
-    new_dict = simple_delete(a_dictionary, 'track')
-    print_sorted_dictionary(a_dictionary)
-    print("--")
-    print_sorted_dictionary(new_dict)
-
-    print("--")
-    print("--")
-
-    new_dict = simple_delete(a_dictionary, 'c_is_fun')
-    print_sorted_dictionary(a_dictionary)
-    print("--")
-    print_sorted_dictionary(new_dict)
+    if a_dictionary is None:
+        return
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
